a435fcdf3f9dee8b484916702727df0bff437b75
Isaac Benitez <ibsandoval@gmail.com>
2025-12-31 15:34:55 -0800

    added key bindings docs to the CLI help

--- README.md
+++ README.md
@@ -38,9 +38,11 @@ echo '{"helo": "world"}' | vj
 `5k` - move cursor 5 lines up from current position<br>
 `g` - move cursor to the first line of the document<br>
 `G` - move cursor to the last line of the document<br>
+`{` - move cursor to the previous sibling
+`}` - move cursor to the next sibling
 
 ### Command Mode
 
 `:` - Switch to commands mode<br>
-`:.` - Find path in JSON for example `:.users[0].email`<br>
-`:q` - Close vj<br>
+`:.` - Find path in JSON, for example `:.users[0].email`<br>
+`:q` - Quit<br>
--- utils.go
+++ utils.go
@@ -12,6 +12,21 @@ Usage: vj [file]
 
 Arguments:
    -h, --help            print help
-   -v, --version         print version`, version,
+   -v, --version         print version
+
+Key bindings:
+   h, ←                  fold JSON object or array
+   l, →                  unfold JSON object or array
+   j, ↓                  move cursor down
+   k, ↑                  move cursor up
+   5j                    move cursor 5 lines down from current position
+   5k                    move cursor 5 lines up from current position
+   {                     move cursor to previous sibling
+   }                     move cursor to next sibling
+   g                     move cursor to the first line of the document
+   G                     move cursor to the last line of the document
+   :                     Switch to command mode
+   :.                    Find path in JSON, for example :.users[0].email
+   :q                    Quit`, version,
 	)
 }
